a0bf2f0a3764c67cfabc143f93fc04195be432d6
Mihail Treschalin <mihail.treschalin@ya.ru>
2022-03-15 23:43:23 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Mihail Treschalin");
+        System.out.println("30.07.1980");
+    }
+}
